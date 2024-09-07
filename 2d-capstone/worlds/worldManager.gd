5518ba2b7b2ee788e4eba192794b79c8101256a7
Kayla <kaylarhan@gmail.com>
2024-09-07 18:52:42 -0500

    Added countdown

--- 2d-capstone/worlds/worldManager.gd
+++ 2d-capstone/worlds/worldManager.gd
@@ -5,6 +5,7 @@ signal resetPosition(who)
 var player1 
 var player2
 var killWall
+var countdownUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,10 +13,22 @@ func _ready():
 	player1 = get_node("Player1")
 	player2 = get_node("Player2")
 	killWall = get_node("KillWall")
-	await get_tree().create_timer(0).timeout
+	countdownUI = get_node("LevelUI")
+	changeCountdown()
+	await get_tree().create_timer(3.0).timeout
 	Globals.inLevel = true
 	pass # Replace with function body.
 
+
+func changeCountdown():
+	await get_tree().create_timer(1.0).timeout
+	countdownUI.get_node("Countdown").text = "2"
+	await get_tree().create_timer(1.0).timeout
+	countdownUI.get_node("Countdown").text = "1"
+	await get_tree().create_timer(1.0).timeout
+	countdownUI.get_node("Countdown").text = "Go!"
+	await get_tree().create_timer(1.0).timeout
+	countdownUI.get_node("Countdown").text = ""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
