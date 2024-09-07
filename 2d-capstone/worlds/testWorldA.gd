e16573063aa6024b3423df94ea4a4c331fff719b
amjchang88 <37486907+andymjchang@users.noreply.github.com>
2024-09-07 15:24:22 -0500

    testWorldA - Andy

---
+++ 2d-capstone/worlds/testWorldA.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var time : float = 0
+var bpm: int = 100
+@onready var timer
+
+func _ready():
+	timer = $Timer
+	
+# autoscroller manager
+func _process(delta: float) -> void:
+	updateTime(delta)
+	
+func updateTime(delta: float):
+	time = time + delta
+	timer.text = str(round_to_dec(time, 2))
+	
+func round_to_dec(num, digit):
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
