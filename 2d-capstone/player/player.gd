41c5e79a47f88ddb7555ed5c69643b4b322d32fc
Kayla <kaylarhan@gmail.com>
2024-09-07 15:08:21 -0500

    added controls for different players

--- 2d-capstone/player/player.gd
+++ 2d-capstone/player/player.gd
@@ -3,9 +3,27 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var inLevel = false
 var attack
+var left
+var right
+var jump
+var punch
 
 func _ready():
+	print("my name is: ",self.name)
+	# Controls for player1
+	if self.name == "Player1":
+		left = "left1"
+		right = "right1"
+		jump = "jump1"
+		punch = "punch1"
+	elif self.name == "Player2":
+		left = "left2"
+		right = "right2"
+		jump = "jump2"
+		punch = "punch2"
+
 	attack = get_node("AttackHitbox")
 	print("attack loaded")
 
@@ -14,20 +32,24 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# If not currently in a song, allow regular movement, otherwise begin autoscroll
+	if !inLevel:
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis(left, right)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		velocity.x = 1.0 * SPEED
+
+	# Other player mechanics
+	if Input.is_action_just_pressed(jump) and is_on_floor():
+		print("Jump!")
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	#velocity.x = 1.0 * SPEED
-
-	if Input.is_action_just_pressed("punch"):
+	if Input.is_action_just_pressed(punch):
 		attack.disabled = false
 		attack.visible = true
 		print("Punch!")
@@ -35,9 +57,5 @@ func _physics_process(delta: float) -> void:
 		print("Punch over")
 		attack.disabled = true
 		attack.visible = false
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		print("Jump!")
-		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
