74ee1733be021f7f0d4cf61364b8d47080df3452
Shadow Sincross <shadowanneruby@gmail.com>
2024-09-08 15:06:20 -0500

    cleaned it up a bit

--- 2d-capstone/enemy/enemy.gd
+++ 2d-capstone/enemy/enemy.gd
@@ -4,13 +4,14 @@ signal takeDamage
 
 var count = 0
 
+var soundPlayer := AudioStreamPlayer.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("enemies")
+	add_child(soundPlayer)
 	pass # Replace with function body.
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -20,6 +21,10 @@ func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_inde
 	if body.is_in_group("players") and body.attack.visible:
 		self.visible = false
 	elif body.is_in_group("players"):
+		#here if we want it lol, doesnt work as of now
+		#var soundEffect = load("res://audioTracks/record-scratches-31350.mp3")
+		#soundPlayer.stream = soundEffect
+		#soundPlayer.play()
 		body.emit_signal("takeDamage",1)
 	pass # Replace with function body.
 	
