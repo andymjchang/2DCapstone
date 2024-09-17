2452e6c2c5590f4f03e6b35b3c1ad8149fc41c45
andymjchang <amjchang88@gmail.com>
2024-09-17 18:23:18 -0500

    preliminary dynamic grid resizing

--- 2d-capstone/levelEditor/measure_lines.gd
+++ 2d-capstone/levelEditor/measure_lines.gd
@@ -3,7 +3,6 @@ extends Node2D
 var measurePixels = 600
 var beatsPerMeasure = 3
 var stepSize = 150  # This will define the spacing for the grid lines
-
 var viewport_width
 var viewport_height
 
@@ -11,23 +10,47 @@ func _ready():
 	viewport_width = get_viewport_rect().size.x
 	viewport_height = get_viewport_rect().size.y
 
+func _process(_delta):
+	# Trigger redraw every frame
+	queue_redraw()
+
 func _draw():
+	var camera = get_viewport().get_camera_2d()
+	if not camera:
+		return
+
+	var zoom = camera.zoom
+	var camera_pos = camera.global_position
+
+	# Calculate visible area
+	var top_left = camera_pos - Vector2(viewport_width, viewport_height) / (2 * zoom)
+	var bottom_right = camera_pos + Vector2(viewport_width, viewport_height) / (2 * zoom)
+
+	# Adjust grid drawing based on zoom
+	var adjusted_step = stepSize * zoom.x
+	var adjusted_measure = measurePixels * zoom.x
+
+	# Calculate grid start and end positions
+	var start_x = floor(top_left.x / adjusted_step) * adjusted_step
+	var end_x = ceil(bottom_right.x / adjusted_step) * adjusted_step
+	var start_y = floor(top_left.y / adjusted_step) * adjusted_step
+	var end_y = ceil(bottom_right.y / adjusted_step) * adjusted_step
+
 	# Draw grid lines
-	for y in range(0, int(viewport_height / stepSize) + 1):
-		var grid_y = y * stepSize
-		draw_line(Vector2(0, grid_y), Vector2(viewport_width, grid_y), Color.GRAY, 2.0)
-	for x in range(0, int(viewport_width / stepSize) + 1):
-		var grid_x = x * stepSize
-		draw_line(Vector2(grid_x, 0), Vector2(grid_x, viewport_height), Color.GRAY, 2.0)
+	for y in range(start_y, end_y, adjusted_step):
+		draw_line(Vector2(start_x, y), Vector2(end_x, y), Color.GRAY, 2.0 / zoom.x)
+
+	for x in range(start_x, end_x, adjusted_step):
+		draw_line(Vector2(x, start_y), Vector2(x, end_y), Color.GRAY, 2.0 / zoom.x)
 
 	# Draw measure lines
-	for x in range(0, int(viewport_width / measurePixels) + 1):
-		var measure_x = x * measurePixels
-		draw_line(Vector2(measure_x, 0), Vector2(measure_x, viewport_height), Color.RED, 2.0)
-		
+	var measure_start_x = floor(top_left.x / adjusted_measure) * adjusted_measure
+	for x in range(measure_start_x, end_x, adjusted_measure):
+		draw_line(Vector2(x, start_y), Vector2(x, end_y), Color.RED, 2.0 / zoom.x)
+
 		# Subdivide each measure based on beatsPerMeasure
 		if beatsPerMeasure > 1:
-			var beat_interval = measurePixels / beatsPerMeasure
+			var beat_interval = adjusted_measure / beatsPerMeasure
 			for b in range(1, beatsPerMeasure):
-				var beat_x = measure_x + b * beat_interval
-				draw_line(Vector2(beat_x, 0), Vector2(beat_x, viewport_height), Color.GREEN, 2.0)
+				var beat_x = x + b * beat_interval
+				draw_line(Vector2(beat_x, start_y), Vector2(beat_x, end_y), Color.GREEN, 2.0 / zoom.x)
