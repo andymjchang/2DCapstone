b6ad0cc7f2a19f8aa4565e1483f88bb12b518b15
Shadow Sincross <shadowanneruby@gmail.com>
2024-09-18 10:43:55 -0500

    size up and size down

--- 2d-capstone/worldObjects/killFloor.gd
+++ 2d-capstone/worldObjects/killFloor.gd
@@ -13,8 +13,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("downsize"):
+		self.scale/=1.05
 	if Input.is_action_just_pressed("sizeUp"):
-		self.scale/=1.01
+		self.scale*=1.05
 	pass
 	
 func _onKillFloorBodyEntered(body:Node2D):
