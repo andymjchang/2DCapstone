5595226e6e7df5fee7f773fbc09528e93682a949
andymjchang <amjchang88@gmail.com>
2024-09-18 02:13:46 -0500

    block correct sprite picking after saving

--- 2d-capstone/worldObjects/blockLong.gd
+++ 2d-capstone/worldObjects/blockLong.gd
@@ -3,11 +3,5 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var randPlatform = "Platform" + str(randi_range(1, 2))
-	get_node(randPlatform).visible = true
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	var randIndex = randi_range(0, 1)
+	$sprite2D.get_children()[randIndex].visible = true
