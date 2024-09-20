948e524ab6ceafbb3c88ac69c7f59e7285bc0b56
Shadow Sincross <shadowanneruby@gmail.com>
2024-09-20 02:56:14 -0500

    goal block works, need to make a clickzone

--- 2d-capstone/shadow/goalBlock.gd
+++ 2d-capstone/shadow/goalBlock.gd
@@ -1,17 +1,12 @@
-extends BaseObject
+extends Node2D
 var reached = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group('goals')
-	self.blockType = "goalBlock"
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 #when both players reach their goal block, the game is ended. 
