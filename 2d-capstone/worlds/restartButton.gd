795d53c77f45e204f9f1a0525ea3fc10d1a3b120
amjchang88 <37486907+andymjchang@users.noreply.github.com>
2024-09-22 20:37:59 -0500

    added editor customizability to levelUI path

--- 2d-capstone/worlds/restartButton.gd
+++ 2d-capstone/worlds/restartButton.gd
@@ -1,22 +1,10 @@
-extends Button
+extends CanvasLayer
 
 @export var goToScenePath = "res://worlds/menu.tscn"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
-func _onPressed():
-	get_tree().change_scene_to_file(goToScenePath)
-	pass # Replace with function body.
-
+@export var reloadSelf = true
 
 func _on_restart_button_button_down() -> void:
-	pass # Replace with function body.
+	if reloadSelf:
+		get_tree().reload_current_scene()
+	else:
+		get_tree().change_scene_to_file(goToScenePath)
