fe81f37d02b38db2e7d8f194271549c3b4be0101
Shadow Sincross <shadowanneruby@gmail.com>
2024-09-24 20:06:49 -0500

    fixed nulling out bug for player 2 health

--- 2d-capstone/ui/healthManager.gd
+++ 2d-capstone/ui/healthManager.gd
@@ -21,19 +21,21 @@ func _process(delta: float) -> void:
 	
 func _onDamageTaken(who) -> void:
 	#do damage on current heart
-	print("damage signal connected")
 	if who == "Player1":
-		curP1Heart.takeDamage()
-		if curP1Heart.healthStatus == "zero" and curP1HeartIndex != 0:
-			#player has lost a heart
-			curP1HeartIndex -= 1
-			curP1Heart = self.get_child(curP1HeartIndex)
+		if curP1Heart:
+			curP1Heart.takeDamage()
+			if curP1Heart.healthStatus == "zero" and curP1HeartIndex != 0:
+				#player has lost a heart
+				curP1HeartIndex -= 1
+				curP1Heart = self.get_node("player1").get_child(curP1HeartIndex)
 	if who == "Player2":
-		curP2Heart.takeDamage()
-		if curP2Heart.healthStatus == "zero" and curP2HeartIndex != 0:
-			#player has lost a heart
-			curP2HeartIndex -= 1
-			curP2Heart = self.get_child(curP2HeartIndex)
+		#need to check to make sure not dead
+		if curP2Heart:
+			curP2Heart.takeDamage()
+			if curP2Heart.healthStatus == "zero" and curP2HeartIndex != 0:
+				#player has lost a heart
+				curP2HeartIndex -= 1
+				curP2Heart = self.get_node("player2").get_child(curP2HeartIndex)
 		
 	
 	
