0d46108e02fe1f4c007bf93a9ebf1fff37aa7cb9
amjchang88 <37486907+andymjchang@users.noreply.github.com>
2024-09-27 18:03:40 -0500

    fixed desync bug

--- 2d-capstone/worlds/camera.gd
+++ 2d-capstone/worlds/camera.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	make_current()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Globals.inLevel:
 		position.x = position.x + bpm * delta
