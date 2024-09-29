bce8a4c4a43ff0c756f78fc5d7f2594a1584eb23
Shadow Sincross <shadowanneruby@gmail.com>
2024-09-28 19:59:33 -0500

    can choose audio

--- 2d-capstone/levelEditor/audioTracks.gd
+++ 2d-capstone/levelEditor/audioTracks.gd
@@ -21,10 +21,11 @@ func fillAudioScroll() -> void:
 		var fileName = audioDir.get_next()
 		while fileName != "":
 			#add audio file to drop down
-			button = Button.new()
-			button.text = fileName
-			button.connect("pressed", _on_button_pressed.bind(fileName))
-			self.get_child(0).add_child(button)
+			if !fileName.ends_with(".import") and !audioDir.current_is_dir():
+				button = Button.new()
+				button.text = fileName
+				button.connect("pressed", _on_button_pressed.bind(fileName))
+				self.get_child(0).add_child(button)
 			
 			fileName = audioDir.get_next()
 			
@@ -34,7 +35,7 @@ func _on_button_pressed(fileName) -> void:
 	var newAudio = load(audioPath) as AudioStream
 	print("audio path: ", audioPath)
 	print("new audio: ", newAudio)
-	self.get_parent().get_parent().get_parent().get_node("objectList/audio").stream = newAudio
-	print("stream: ", self.get_parent().get_parent().get_parent().get_node("objectList/audio").stream )
+	self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream = newAudio
+	print("stream: ", self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream )
 			
 	
