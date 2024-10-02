ddbef8b8d773f3bea2789a80ff650fd12e844e32
Shadow Sincross <shadowanneruby@gmail.com>
2024-10-01 20:02:20 -0500

    progress bar moves when enemies hit

--- 2d-capstone/worldObjects/enemy/enemy.gd
+++ 2d-capstone/worldObjects/enemy/enemy.gd
@@ -39,6 +39,8 @@ func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_inde
 	if body.is_in_group("players") and body.attack.visible:
 		self.ifDead = true
 		print("enemy punched")
+		get_parent().get_parent().get_parent().get_node("ScoreBar/TextureProgressBar").emit_signal("increaseScore")
+		print("bar, ", get_parent().get_parent().get_parent().get_node("ScoreBar"))
 	elif body.is_in_group("players"):
 		#here if we want it lol, doesnt work as of now
 		#var soundEffect = load("res://audioTracks/record-scratches-31350.mp3")
