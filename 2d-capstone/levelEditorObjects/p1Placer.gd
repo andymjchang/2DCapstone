75fc643917dd2df47816141f03045458601a530b
Shadow Sincross <shadowanneruby@gmail.com>
2024-10-07 13:29:46 -0500

    placers reflect coords placed, still misalgined line

--- 2d-capstone/levelEditorObjects/p1Placer.gd
+++ 2d-capstone/levelEditorObjects/p1Placer.gd
@@ -9,7 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#dont let the player placer objects get out of line
+	print("line pos: ", lineNode.global_position)
+	var index = 1
 	for placer in self.get_children():
 		if placer.name != "Line":
-			#placer.global_position.x = lineNode.global_position.x
-			print("placer global pos: ", placer.global_position, " name: ", placer.name)
+			placer.global_position.x = lineNode.global_position.x
+			var editorName = "EditorArea"+str(index)
+			index+=1
+	Globals.startP1Coords = self.get_node("Player1").global_position
+	Globals.startP2Coords = self.get_node("Player2").global_position
