df2c9261a58909840c0504afd0bde777fdd2a721
Shadow Sincross <shadowanneruby@gmail.com>
2024-10-06 20:37:34 -0500

    not file-saving placers

--- 2d-capstone/levelEditor/levelEditor.gd
+++ 2d-capstone/levelEditor/levelEditor.gd
@@ -364,14 +364,16 @@ func save_scene_to_file():
 			var newFile = FileAccess.open("res://levelData/" + saveFileName + ".dat", 7)
 			for itemList in objectList.get_children():
 				newFile.store_string(itemList.name + "\n")
-				for item in itemList.get_children():
-					#go through each of the items children areas
-					var childrenList = item.get_child(0).get_children()
-					for blockChild in childrenList:
-						#this is most likely where the zipline error/duplication is occuring
-						print("block child being saved, ", blockChild.get_child(0))
-						newFile.store_string(str(blockChild.get_child(0).global_position.x) + ", " + str(blockChild.get_child(0).global_position.y) + "\n")
-					#print("child list in save, ", childrenList)
+				if itemList.name !=  "placers":
+					print("item name: ", itemList.name, " ", blockTypes[11])
+					for item in itemList.get_children():
+						#go through each of the items children areas
+						var childrenList = item.get_child(0).get_children()
+						for blockChild in childrenList:
+							#this is most likely where the zipline error/duplication is occuring
+							print("block child being saved, ", blockChild.get_child(0))
+							newFile.store_string(str(blockChild.get_child(0).global_position.x) + ", " + str(blockChild.get_child(0).global_position.y) + "\n")
+						#print("child list in save, ", childrenList)
 					
 	else:
 		displayStatus(UNABLE_TO_SAVE, false)
