9862e999e39ac6a52b8d07f9d23efa078ef0a2ea
amjchang88 <37486907+andymjchang@users.noreply.github.com>
2024-10-08 20:46:03 -0500

    Update enemy.gd

--- 2d-capstone/worldObjects/enemy/enemy.gd
+++ 2d-capstone/worldObjects/enemy/enemy.gd
@@ -51,7 +51,7 @@ func DeathAnimation(delta: float) -> void:
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("players") and body.attack.monitoring:
 		self.ifDead = true
-		velocity.y = randi_range(-900, -400)
+		velocity.y = randi_range(-600, -300)
 		sprite.rotation = randf_range(min_rotation, max_rotation)
 		get_parent().get_parent().get_parent().get_node("ScoreBar/TextureProgressBar").emit_signal("increaseScore")
 	elif body.is_in_group("players"):
