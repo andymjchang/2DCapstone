026d645469d136979af8fb29c35379ec98318280
Shadow Sincross <shadowanneruby@gmail.com>
2024-10-16 13:22:15 -0500

    lvl2 tilemap for slide wall

--- 2d-capstone/worldObjects/scripts/blockLong.gd
+++ 2d-capstone/worldObjects/scripts/blockLong.gd
@@ -9,6 +9,9 @@ func _ready():
 	if Globals.curFile.begins_with("Lvl2."):
 		$sprite2D/TileMapLayer.visible = false
 		$sprite2D/TileMapLayer2.visible = true
+	else:
+		$sprite2D/TileMapLayer.visible = true
+		$sprite2D/TileMapLayer2.visible = false
 	#var randIndex = randi_range(0, 1)
 	#$sprite2D.get_children()[randIndex].visible = true
 	#activeSprite = 	$sprite2D.get_children()[randIndex]
