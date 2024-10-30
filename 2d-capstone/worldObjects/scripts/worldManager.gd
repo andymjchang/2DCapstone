c9859754b45b3a3a269eb44b6b9c8db1139c3993
andymjchang <amjchang88@gmail.com>
2024-10-30 15:25:52 -0500

    Update worldManager.gd

--- 2d-capstone/worldObjects/scripts/worldManager.gd
+++ 2d-capstone/worldObjects/scripts/worldManager.gd
@@ -79,7 +79,7 @@ func _ready():
 		Globals.currentSongFileName = "Level2_Main_156bpm_V2.mp3"
 		backgroundName = "Lvl2"
 		
-	if levelFile.begins_with("Lvl0."):
+	if levelFile.begins_with("Tutorial"):
 		var popUpScene = load("res://worldObjects/onboardingPopUp.tscn")
 		var popUpInstance = popUpScene.instantiate()
 		$Camera2D.add_child(popUpInstance)
