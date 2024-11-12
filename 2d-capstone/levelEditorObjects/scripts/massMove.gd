c28ea8887e9c3186a25614f512196136fb6c7d60
Shadow Sincross <shadowanneruby@gmail.com>
2024-11-12 15:41:59 -0600

    added color coding

--- 2d-capstone/levelEditorObjects/scripts/massMove.gd
+++ 2d-capstone/levelEditorObjects/scripts/massMove.gd
@@ -12,8 +12,8 @@ var axisType = "vertical"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	startSprite.modulate = Color(0, 1, 0)
+	endSprite.modulate = Color(1, 0, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
