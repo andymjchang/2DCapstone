899e7bb9c772e40961ec91628f6aea5c54f531fb
andymjchang <amjchang88@gmail.com>
2024-11-16 21:57:33 -0600

    redid assets

--- 2d-capstone/worldObjects/enemy/holdEnemy.gd
+++ 2d-capstone/worldObjects/enemy/holdEnemy.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var headSprite = $SlideEnemyHead
 @onready var bodySprites = $Segments
 @onready var tailSprite = $SlideEnemyEnd
+@onready var bodySprite = $Segments/SlideEnemyMiddle
 
 var squash_speed: float = 20.0  # Adjust this value to control squashing speed
 var is_squashing: bool = false
@@ -12,32 +13,41 @@ var original_distance: float
 func _ready() -> void:
 	var tail_pos = tailSprite.position
 	var head_pos = headSprite.position
+	original_distance = abs(head_pos.x - tail_pos.x)
+	
+	# Calculate number of body segments needed
+	var segment_width = 300
+	var num_segments = ceil(original_distance / segment_width)
+	print("num_segments: ", num_segments)
+	# Create and position body segments
+	for i in range(num_segments):
+		var new_segment = bodySprite.duplicate()
+		bodySprites.add_child(new_segment)
+		new_segment.position.x = (i * segment_width) - (original_distance / 2)
+		new_segment.visible = true
+
 	bodySprites.position.x = (head_pos.x + tail_pos.x) / 2.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		start_squash()
-		print("squash")
 	if is_squashing:
-		# Move head towards tail
 		var tail_pos = tailSprite.position
 		var head_pos = headSprite.position
 		headSprite.position.x += squash_speed * delta
 		
-		# Calculate the distance between head and tail
 		var distance = abs(head_pos.x - tail_pos.x)
 		
-		# Stop squashing if head is very close to tail
-		if distance <= headSprite.get_node("Head").texture.get_width() / 4:  # You can adjust this minimum distance
+		if distance <= headSprite.get_node("Head").texture.get_width() / 4:
 			is_squashing = false
 			return
 			
 		headSprite.position.x += squash_speed * delta
 		
 		# Update body segments scale and position
-		print(distance)
-		bodySprites.scale.x = distance / original_distance
+		var scale_factor = distance / original_distance
+		bodySprites.scale.x = scale_factor
+		
+		# Update position of body segments container
 		bodySprites.position.x = (head_pos.x + tail_pos.x) / 2.0
 
 func start_squash():
