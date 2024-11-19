9b17a975d31d18f847814c17f3948ac7f3dbceec
Kayla <kaylarhan@gmail.com>
2024-11-19 16:55:40 -0600

    hitbox adjustment when on zip

--- 2d-capstone/worldObjects/zipline/zipline.gd
+++ 2d-capstone/worldObjects/zipline/zipline.gd
@@ -21,7 +21,7 @@ func _onBodyEntered(body):
 			var start = get_parent().get_child(START).get_node("Marker2D")
 			var direction = (destination.global_position - start.global_position).normalized()
 			body.position.y = start.get_parent().get_node("PlayerMarker").global_position.y
-				
+			body.get_node("AttackHitbox/CollisionShape2D").scale = Vector2(1, 3)
 			#body.position.y = start.get_parent().get_node("playerMarker").global_position.y
 			print("Destination: ", destination.global_position)
 			print("Velocity: ", direction)
@@ -40,6 +40,7 @@ func _onBodyEntered(body):
 			body.relocating = false
 			body.get_node("Animation").play("Run")
 			body.position.y -= 100
+			body.get_node("AttackHitbox/CollisionShape2D").scale = Vector2(1, 1)	
 			body.velocity = Vector2(0, 0)
 			Globals.vertical = false
 			Globals.resetCamera = true
