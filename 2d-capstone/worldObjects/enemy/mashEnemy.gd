d15faac335a419b9583afe8740f840b63de119d1
andymjchang <amjchang88@gmail.com>
2024-11-18 20:18:29 -0600

    mashEnemy speed during slow mode fixed

--- 2d-capstone/worldObjects/enemy/mashEnemy.gd
+++ 2d-capstone/worldObjects/enemy/mashEnemy.gd
@@ -57,7 +57,7 @@ func _ready() -> void:
 	timeRemaining = activeTime
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if ifDead:	
 		DeathAnimation(delta)
 	elif isFirstHit:
