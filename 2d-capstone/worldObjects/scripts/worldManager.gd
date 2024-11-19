6d8701f9bc08dfafd695b18e684fd479a02e95ef
jhale24 <jhale24@utexas.edu>
2024-11-19 02:40:39 -0600

    Custom Level Done + Cleaning

--- 2d-capstone/worldObjects/scripts/worldManager.gd
+++ 2d-capstone/worldObjects/scripts/worldManager.gd
@@ -96,6 +96,10 @@ func _ready():
 		Globals.currentSongFileName = "Level2_OGNoMelody_156bpm_1.mp3"
 		adaptiveMusic.active = true
 		backgroundName = "Lvl2"
+	if levelFile.begins_with("CustomLevel"):
+		Globals.setBPM(160)
+		Globals.currentSongFileName = "CustomLevel_160bpm.wav"
+		backgroundName = "LvlCustom"
 
 	Globals.gameOver = false
 	Globals.inLevel = false
