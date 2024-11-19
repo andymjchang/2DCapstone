468d1748567fa5494f283c9469a0af966ce2907a
Shadow Sincross <shadowanneruby@gmail.com>
2024-11-18 20:24:42 -0600

    deltes two

--- 2d-capstone/levelEditorObjects/platformBlockScene.gd
+++ 2d-capstone/levelEditorObjects/platformBlockScene.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name platformBlockScene
 #6 cols  default
 @onready var tileMap = self.get("Node2D/TileMapLayer")
+@onready var base = $Node2D/base
 #this is the default for now
 var numCols = 20
 var extents
@@ -83,20 +84,55 @@ func decreaseByOneTile() -> void:
 		print("start coords ",startX," , ", startY)
 		print("end coords ",endX," , ", endY)
 		#deleting the end, might need to do this twice
-		for i in range(startY,endY+1):
-			print("deleting coords: ",endX," , ", moveY)
-			tileMap.erase_cell(Vector2i(endX, moveY))
-			moveY+=1
 		
+		#we want to delete two rows, but not starting at the end
+		for i in range (0,2):
+			var curX = endX - (i +3)
+			moveY = startY
+			for j in range(startY,endY+1):
+				print("deleting coords: ",curX," , ", moveY)
+				tileMap.erase_cell(Vector2i(curX, moveY))
+				moveY+=1
+		
+		
+		#now we need to shift the current end by 2 blocks
 		#setting the X to the new end
-		endX-=1
+		#endX-=1
 		startY = minMax[0].y
-		
+		moveY = startY
 		#add in end cap
-		#for i in range (0,endY):
-			#tileMap.set_cell(Vector2i(endX, startY), 1, endTiles[i])
-			#startY+=1
+		var endStart = endTiles[1][0].y
+		var endEnd = endTiles[1][endTiles[1].size()-1].y
+		
+		
+		#the way end tiles is set up
+		#  col 1 col 2 col 3
+		#   1     1     1
 		#
+		for i in range(0,3):
+			#starts at the furthest right box of the end tiles
+			var curX = endX - i
+			var curCol = endTiles[i]
+			var colIndex = 0
+			for j in range(startY,endY+1):
+				var atlasCoords = tileMap.get_cell_atlas_coords(Vector2i(curX, j))
+				tileMap.set_cell(Vector2i(curX-2, j-1), 1, Vector2i(curX, j))
+				tileMap.erase_cell(Vector2i(curX, j))
+				#tileMap.set_cell(Vector2i(curX, startY), 1, curCol[colIndex])
+				#colIndex+=1
+		#for i in 2:
+			#var curEndSet = endTiles[i]
+			#print("end tiles we are setting: ", endTiles[i])
+			#tileMap.set_cell(Vector2i(endX-1, moveY), 1, curEndSet[0])
+			#tileMap.set_cell(Vector2i(endX, moveY), 1, curEndSet[1])
+			#moveY+=1
+		#
+		#moveY = endEnd+2
+		#for i in range(endEnd+2, endY+1):
+			#tileMap.erase_cell(Vector2i(endX, moveY))
+			#moveY+=1
+			
+		
 		
 		self.get_node("Node2D/EditorArea0/%CollisionShape2D").shape.extents.x -= tileWidth/2.0
 		self.get_node("Node2D/EditorArea0").global_position.x -= tileWidth/2.0
@@ -138,7 +174,7 @@ func setStartTiles() -> void:
 	#the start blocks are only 1 tile wide, so we just need to 
 	#get how long it is
 	#TODO minmize doing this
-	var usedCells = tileMap.get_used_cells()
+	var usedCells = base.get_used_cells()
 	var maxMin = getMaxMinCoord(usedCells)
 	var maxY = maxMin[1].y
 	var startX = maxMin[0].x
@@ -150,23 +186,37 @@ func setStartTiles() -> void:
 		startTiles.append(Vector2(startX,i))
 
 func setEndTiles() -> void:
-	var usedCells = tileMap.get_used_cells()
+	var usedCells = base.get_used_cells()
 	var maxMin = getMaxMinCoord(usedCells)
 	var maxY = maxMin[1].y
-	var startX = maxMin[1].x
+	var minY = maxMin[0].y
+	var endX = maxMin[1].x
+	
 	
 	#reset the array
+	#it has to hold both cols
+	print("max y: ", maxY, " minY:", minY)
+	
 	endTiles = []
-	for i in maxY:
-		#we need to grab the end tiles, and store them 
-		endTiles.append(Vector2(startX,i))
+	var curCol = []
+	
+	var allCols = []
+	#the end tiles are the last three rows
+	for i in range(0,3):
+		var curX = endX - i
+		curCol = []
+		for j in range(minY, maxY+1):
+			#we need to grab the end tiles, and store them 
+			curCol.append(Vector2(curX,i))
+		allCols.append(curCol)
+	endTiles = allCols
 		
 func setFillerTiles() -> void:
 	#we do everything in twos
 	var arrayOne : Array
 	var arrayTwo : Array
 	#we dont grab the start or end tiles
-	var usedCells = tileMap.get_used_cells()
+	var usedCells = base.get_used_cells()
 	var maxMin = getMaxMinCoord(usedCells)
 	var minX = maxMin[0].x
 	var maxX = maxMin[1].x
