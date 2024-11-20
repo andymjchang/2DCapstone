6f0117c25e80f315c4708890e8cbd48118af8623
kayreo <kayla.han@g.austincc.edu>
2024-11-20 04:12:58 -0600

    protect file names

--- 2d-capstone/levelEditor/levelEditor.gd
+++ 2d-capstone/levelEditor/levelEditor.gd
@@ -696,9 +696,10 @@ func setFileLoad():
 
 
 func _on_load_file_pressed() -> void:
+	$UI/FileLoadMode/StatusMessage.text = "Enter a file name."
 	var tgtFile = $UI/FileLoadMode/FileName.text
 	print("Tgt:" , tgtFile)
-	if "Level 1" in tgtFile or "Level 2" in tgtFile or "Level 3" in tgtFile:
+	if "level 1" in tgtFile.to_lower() or "level 2" in tgtFile.to_lower() or "level 3" in tgtFile.to_lower():
 		$UI/FileLoadMode/StatusMessage.text = "Cannot open file."
 	elif FileAccess.file_exists(levelDataPath + tgtFile + ".dat"):
 		saveFileName = tgtFile
