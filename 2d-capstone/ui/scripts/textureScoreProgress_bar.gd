1b66957dbd4c4ec74e718c899e122d21edc302e2
andymjchang <amjchang88@gmail.com>
2024-12-02 03:47:21 -0600

    dynamic loading for new texture progress bars

--- 2d-capstone/ui/scripts/textureScoreProgress_bar.gd
+++ 2d-capstone/ui/scripts/textureScoreProgress_bar.gd
@@ -4,17 +4,30 @@ signal increaseScore
 var numEnemies = 0.0
 var enemiesHit = 0.0
 
+var textureBar
+
 
 func _ready() -> void:
+	AssignTextureBar()
 	numEnemies = self.get_parent().get_parent().get_node("objectList/enemies").get_child_count()
 	self.increaseScore.connect(_scoreIncrease)
 	# Initialize the progress bar values
-	self.min_value = 0
-	self.max_value = 100
-	self.value = 0  # Start at zero
+	textureBar.min_value = 0
+	textureBar.max_value = 100
+	textureBar.value = 0  # Start at zero
+
+func AssignTextureBar() -> void:
+	if Globals.curFile == "Level 1" or Globals.curFile.begins_with("Tutorial") or Globals.curFile == "Level 2":
+		textureBar = get_node("Level1")
+	elif Globals.curFile == "Level 3":
+		textureBar = get_node("Level2")
+	else:
+		textureBar = get_node("Level3")
+
+	textureBar.visible = true
 
 func _scoreIncrease() -> void:
-	self.value = self.value + 1
+	textureBar.value = textureBar.value + 1
 	enemiesHit += 1
 	Globals.percentageHit = (enemiesHit/numEnemies) * 100
 	
