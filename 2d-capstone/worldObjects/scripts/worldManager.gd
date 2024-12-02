4694132cb251df48cd6e5eed4a7cc2ea132bdfa3
jhale24 <jhale24@utexas.edu>
2024-12-01 19:21:52 -0600

    2/3ish Boss Level
    Gonna eat dinner then finish

--- 2d-capstone/worldObjects/scripts/worldManager.gd
+++ 2d-capstone/worldObjects/scripts/worldManager.gd
@@ -114,6 +114,9 @@ func _ready():
 		Globals.setBPM(160)
 		Globals.currentSongFileName = "CustomLevel_Shifted_160bpm.wav"
 		backgroundName = "Lvl3"
+	if levelFile.begins_with("BossLevel"):
+		Globals.setBPM(165)
+		Globals.currentSongFileName = "BossLevel_FINAL_165bpm.mp3"
 
 	Globals.gameOver = false
 	Globals.inLevel = false
