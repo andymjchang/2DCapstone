1f5352e1352be3c37b8af785f722ba2278b960f1
andymjchang <amjchang88@gmail.com>
2024-12-04 16:13:45 -0600

    fixed corrupted resources

--- 2d-capstone/globals/globals.gd
+++ 2d-capstone/globals/globals.gd
@@ -63,8 +63,8 @@ var currentSongFileName
 
 # Nodes
 var previewNode
-@onready var editorNode = preload("res://levelEditor/levelEditor.tscn")
-@onready var screenFlashNode = preload("res://screenEffects/screenFlashEffect.tscn")
+@onready var editorNode = load("res://levelEditor/levelEditor.tscn")
+@onready var screenFlashNode = load("res://screenEffects/screenFlashEffect.tscn")
 @onready var vignette = $Vignette/ColorRect
 @onready var glitch = $Glitch/TransitionRect
 @onready var screenFlashTimer = $ScreenFlashTimer
