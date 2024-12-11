e6be896cef96211331156cea77d460f1abb3bc2a
kayreo <kayla.han@g.austincc.edu>
2024-12-11 15:15:50 -0600

    audio loading

--- 2d-capstone/levelEditor/audioTracks.gd
+++ 2d-capstone/levelEditor/audioTracks.gd
@@ -4,6 +4,11 @@ var scrollBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var dir = DirAccess.open("user://")
+	dir.make_dir("editorAudio")
+	for buttonChild in get_child(0).get_children():
+		if "Button" in buttonChild.name:
+			buttonChild.connect("pressed", _on_button_pressed.bind(buttonChild.text))
 	fillAudioScroll()
 	pass # Replace with function body.
 
@@ -14,17 +19,18 @@ func _process(delta: float) -> void:
 	
 	
 func fillAudioScroll() -> void:
-	var audioDirPath = "res://audioTracksEditor/"
+	var audioDirPath = "user://editorAudio/"
 	var audioDir = DirAccess.open(audioDirPath)
 	if(audioDir):
 		audioDir.list_dir_begin()
 		var fileName = audioDir.get_next()
 		while fileName != "":
+			#print("File from user: ", fileName)
 			#add audio file to drop down
 			if !fileName.ends_with(".import") and !audioDir.current_is_dir():
 				button = Button.new()
 				button.text = fileName
-				button.connect("pressed", _on_button_pressed.bind(fileName))
+				button.connect("pressed", _on_button_pressedUSER.bind(fileName))
 				self.get_child(0).add_child(button)
 			
 			fileName = audioDir.get_next()
@@ -32,11 +38,20 @@ func fillAudioScroll() -> void:
 			
 func _on_button_pressed(fileName) -> void:
 	Globals.currentSongFileName = fileName
-	var audioPath = "res://audioTracksEditor/" + fileName
+	var audioPath = "res://editorAudio/" + fileName
 	var newAudio = load(audioPath) as AudioStream
-	print("audio path: ", audioPath)
-	print("new audio: ", newAudio)
+	#print("audio path: ", audioPath)
+	#print("new audio: ", newAudio)
 	self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream = newAudio
-	print("stream: ", self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream )
+	#print("stream: ", self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream )
 			
-	
+func _on_button_pressedUSER(fileName) -> void:
+	Globals.currentSongFileName = fileName
+	var audioPath = "user://editorAudio/" + fileName
+	var file = FileAccess.open(audioPath, FileAccess.READ)
+	#print("File opened: ", file)
+	var sound = AudioStreamMP3.new()
+	sound.data = file.get_buffer(file.get_length())
+	self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream = sound
+	#print("stream: ", self.get_parent().get_parent().get_parent().get_node("Camera2D/audio").stream )
+			
